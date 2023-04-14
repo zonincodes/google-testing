ef9bfac4c6e6be1ca0cc50741ce558df3a60fef0
zonin <business.kariukimartin@gmail.com>
2023-04-14 11:08:06 +0300

    test

--- tests/ds_test.cpp
+++ tests/ds_test.cpp
@@ -1,12 +1,21 @@
 #include <gtest/gtest.h>
 #include <data_structures/data_structures.h>
 
+
 TEST(DsTests, TestName_Henry)
 {
     Person jimmy{"Jimmy", 23};
     auto expected = "Jimmy";
     auto result = jimmy.getname();
     ASSERT_EQ(*expected, *result);
+}
+
+TEST(ArrayTests, TestArray)
+{
+    std::array<int, 4> fib{0, 1, 2, 0};
+    int fib_front = fib.front();
+    int expcted = 1;
+    ASSERT_EQ(fib.front(), 0);
 }
 
 int main(int argc, char **argv)
