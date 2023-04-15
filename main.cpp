93c3457bc6d6b3a8ca1b9662cb6330462c2f2597
zonin <business.kariukimartin@gmail.com>
2023-04-15 12:17:04 +0300

    unorderdlist test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <data_structures/data_structures.h>
 int main(){
     std::cout << "Main loding\n" ;
-    Person peris{"Peris", 21};
     std::cout << "Name: " << peris.getname() << "\n";
     return 0;
 }
