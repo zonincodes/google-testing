a68a2d86d9f4d7e4d864513d78bcc142a9967898
zonin <business.kariukimartin@gmail.com>
2023-04-17 13:16:04 +0300

    fix: typo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <data_structures/data_structures.h>
+#include <fstream>
+
+using namespace std;
 int main(){
     ostreamTest();
+    ofstream file{ "lunchtime.txt", ios::out|ios::app};
+
+    file << "Time is an illusion." << endl;
+    file << "Lunch time, " << 2 << "x so." << endl;
+
     return 0;
 }
