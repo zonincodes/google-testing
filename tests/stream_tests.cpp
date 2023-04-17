6c7e3a6bafd9fba435a478a1ae906a4f3ce4cb34
zonin <business.kariukimartin@gmail.com>
2023-04-17 10:10:30 +0300

    stream produce strings with str

--- tests/stream_tests.cpp
+++ tests/stream_tests.cpp
@@ -1,8 +1,26 @@
 #include <gtest/gtest.h>
+#include <string>
+#include <sstream>
 
 TEST(Zero, RandomTest)
 {
     EXPECT_EQ(1, 1);
+}
+
+TEST(OstringStream, ProducesStringsWithStr)
+{
+    std::ostringstream ss;
+
+    ss << "By Grabthar's hammer, ";
+    ss << "by the suns of Worvan. ";
+    ss << "You shall be avenged.";
+
+    const auto lazarous = ss.str();
+    ss.str("I am Groot.");
+    const auto groot = ss.str();
+
+    EXPECT_EQ(lazarous, "By Grabthar's hammer, by the suns of Worvan. You shall be avenged.");
+    EXPECT_EQ(groot, "I am Groot.");
 }
 
 
