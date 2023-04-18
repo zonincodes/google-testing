0d33c2c3d260922e3d7a08375a69213bd4130aea
zonin <business.kariukimartin@gmail.com>
2023-04-18 06:44:49 +0300

    inital filesystems Inspecting files types

--- tests/filesystems_test.cpp
+++ tests/filesystems_test.cpp
@@ -9,7 +9,7 @@ TEST(Test, Zero)
 A file is a filesystem object that supports unput and ouput and holds data. 
 */
 
-TEST(FileSutems, ConstructTwoPaths)
+TEST(FileSystems, ConstructTwoPaths)
 {
     std::filesystem::path empty_path;
     std::filesystem::path shadow_path{"/etc/shadow"};
@@ -18,6 +18,12 @@ TEST(FileSutems, ConstructTwoPaths)
     EXPECT_EQ(shadow_path, "/etc/shadow");
 }
 
+TEST(FileSystems, IspectingFilesType)
+{
+    std::filesystem::path nix_path{R"(/bin/bash)"};
+    EXPECT_TRUE(std::filesystem::is_regular_file(nix_path));
+    EXPECT_FALSE(std::filesystem::is_directory(nix_path));
+}
 int main (int argc, char**  argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
