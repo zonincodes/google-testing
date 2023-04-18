066f8c75f1852055e17b8e3ca9c7bf83dc7db1cf
zonin <business.kariukimartin@gmail.com>
2023-04-18 07:37:02 +0300

    any_of test

--- tests/algo_tests.cpp
+++ tests/algo_tests.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 
+
+// all of test
 TEST(AllOf, AllOf)
 {
     vector<string> words{"Auntie", "Anne's", "alligator"};
@@ -26,7 +28,27 @@ TEST(AllOf, AllOf)
 }
 
 
+// any_of
+// determines Whether any elments in a sequence meets user-specified criteria
 
+// the algorithm returns false if the target sequnce is empty or if pred s true for any element;
+
+TEST(AnyOf, AnyOfTheElementIsTrue)
+{
+    vector<string> words{"Barber", "baby", "bubbles"};
+
+    const auto contains_bar = [](const auto& word){
+        return word.find("Bar") != string::npos;
+    };
+
+    EXPECT_TRUE(any_of(words.cbegin(), words.cend(), contains_bar));
+
+    const auto is_empty = [](const auto& word){
+        return word.empty();
+    };
+
+    EXPECT_FALSE(any_of(words.cbegin(), words.cend(), is_empty));
+}
 
 
 int main(int argc, char** argv)
