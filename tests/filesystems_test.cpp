c8983b7216fa6e6e9924214b0801ce536bc2d34f
zonin <business.kariukimartin@gmail.com>
2023-04-18 05:33:12 +0300

    inital filesystems constructing paths

--- tests/filesystems_test.cpp
+++ tests/filesystems_test.cpp
@@ -1,8 +1,21 @@
 #include <gtest/gtest.h>
-
+#include <filesystem>
 TEST(Test, Zero)
 {
     EXPECT_EQ(1, 1);
+}
+
+/*
+A file is a filesystem object that supports unput and ouput and holds data. 
+*/
+
+TEST(FileSutems, ConstructTwoPaths)
+{
+    std::filesystem::path empty_path;
+    std::filesystem::path shadow_path{"/etc/shadow"};
+
+    EXPECT_TRUE(empty_path.empty());
+    EXPECT_EQ(shadow_path, "/etc/shadow");
 }
 
 int main (int argc, char**  argv)
