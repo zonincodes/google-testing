391580cfa9035b1d5b1398ff288af2c4f1fc6696
zonin <business.kariukimartin@gmail.com>
2023-04-18 07:25:55 +0300

    all_of test

--- tests/algo_tests.cpp
+++ tests/algo_tests.cpp
@@ -1,4 +1,33 @@
 #include <gtest/gtest.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+TEST(AllOf, AllOf)
+{
+    vector<string> words{"Auntie", "Anne's", "alligator"};
+
+    const auto starts_with_a = [](const auto& word){
+        if(word.empty()) return false;
+        return word[0] == 'A' || word[0] == 'a';
+    };
+
+    EXPECT_TRUE(all_of(words.cbegin(), words.cend(), starts_with_a));
+    
+
+    //
+    const auto has_length_six = [](const auto& word){
+        return word.length() == 6;
+    };
+
+    EXPECT_FALSE(all_of(words.cbegin(), words.cend(), has_length_six));
+}
+
+
+
+
 
 int main(int argc, char** argv)
 {
