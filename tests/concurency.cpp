6f6e7c228d2bf6b5d009222c1c08da81abba91e7
zonin <business.kariukimartin@gmail.com>
2023-04-20 11:51:47 +0300

    Asynchronous, Wait_for indicates is ready

--- tests/concurency.cpp
+++ tests/concurency.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
 #include <future>
 #include <string>
-
+#include <chrono>
+#include <thread>
 using namespace std;
 
 TEST(Asynchronous, ReturnsValidFuture)
@@ -32,6 +33,18 @@ TEST(Asynchronous, GetMayThrow)
     auto ghostrider = async([]{ throw runtime_error{"The pattern is full."};});
 
     EXPECT_THROW(ghostrider.get(), runtime_error);
+}
+
+// *** wait_for ***
+
+TEST(Asynchronous, WaitForIndicatesTaskIsReady)
+{
+    using namespace literals::chrono_literals;
+    auto sleepy = async(launch::async, [] {this_thread::sleep_for(100ms); });
+    const auto not_ready_yet = sleepy.wait_for(25ms);
+    EXPECT_EQ(not_ready_yet, future_status::timeout);
+    const auto totally_ready = sleepy.wait_for(100ms);
+    EXPECT_EQ(totally_ready , future_status::ready);
 }
 
 int main(int argc, char** argv)
